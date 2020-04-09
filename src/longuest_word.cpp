55a4b24fbd298a05f937811725fd9da6008ddd3f
Dorian Haglund <dorian.haglund@gmail.com>
2020-04-09 18:14:16 +0200

    -

--- src/longuest_word.cpp
+++ src/longuest_word.cpp
@@ -3,7 +3,6 @@
 #include <range/v3/algorithm/any_of.hpp>
 #include <range/v3/algorithm/max_element.hpp>
 #include <range/v3/view/all.hpp>
-#include <string>
 #include <string_view>
 #include <unordered_set>
 
@@ -30,7 +29,7 @@ bool is_madeup(string_view const & word, string_view const & original_word, Dict
     return r::any_of(word_indexes, [&] (auto const i) {
 	auto const & prefix = word.substr(0, i + 1);
 	auto const & suffix = word.substr(i + 1);
-	return can_find(dict, string(prefix)) && is_madeup(suffix, original_word, dict);
+	return can_find(dict, prefix) && is_madeup(suffix, original_word, dict);
     });
 }
 
