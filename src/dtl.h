0ea16f38f532482d7fe38bd4d02d1a00487c538c
Dorian Haglund <dorian.haglund@gmail.com>
2020-04-09 18:07:27 +0200

    -

--- src/dtl.h
+++ src/dtl.h
@@ -1,6 +1,17 @@
 #pragma once
 
-template<typename Cont, typename V>
-bool can_find(Cont const & dict, V const & value) {
+#include <algorithm>
+
+namespace dtl {
+
+template<typename cont_type, typename V>
+bool can_find(cont_type const & dict, V const & value) {
     return dict.find(value) != dict.end();
+}
+
+template<typename cont_type, typename pred_fn>
+auto any_of(cont_type const & c, pred_fn fn) {
+    return std::any_of(begin(c), end(c), fn);
+}
+
 }
--- src/longuest_word.cpp
+++ src/longuest_word.cpp
@@ -1,10 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <range/v3/algorithm/any_of.hpp>
+#include <range/v3/algorithm/max_element.hpp>
+#include <range/v3/view/all.hpp>
 #include <string>
 #include <string_view>
 #include <unordered_set>
 
 #include "dtl.h"
 
+#include "range/v3/algorithm.hpp"
+#include "range/v3/range.hpp"
+#include "range/v3/view.hpp"
+
 using namespace std;
+using namespace dtl;
+
+namespace r = ranges;
+namespace rv = ranges::views;
+namespace ra = ranges::actions;
 
 using Dict = unordered_set<string>;
 
@@ -12,29 +26,51 @@ bool is_madeup(string_view const & word, string_view const & original_word, Dict
     if(can_find(dict, string(word)) && word != original_word)
 	return true;
 
-    for(int i = 0; i < word.size(); i++) {
+    auto word_indexes = rv::iota(0u, word.size());
+    return r::any_of(word_indexes, [&] (auto const i) {
 	auto const & prefix = word.substr(0, i + 1);
 	auto const & suffix = word.substr(i + 1);
-	if(can_find(dict, string(prefix)) && is_madeup(suffix, original_word, dict))
-	    return true;
-    }
-
-    return false;
+	return can_find(dict, string(prefix)) && is_madeup(suffix, original_word, dict);
+    });
 }
 
 bool is_madeup(string_view const & word, Dict const & dict) {
     return is_madeup(word, word, dict);
 }
 
-string_view longuest_word(Dict const & dict) {
-    string_view longuest_w;
-    for(string_view cand: dict) {
-	if(is_madeup(cand, dict) && cand.size() > longuest_w.size())
-	    longuest_w = cand;
-    }
-    return longuest_w;
+optional<string> longuest_madeup_word(Dict const &dict) {
+  auto made_up_words = dict | rv::filter([&](auto const &word) { return is_madeup(word, dict); });
+
+  auto is_less = [](auto const &a, auto const &b) { return a.size() < b.size(); };
+
+  auto longuest = r::max_element(made_up_words, is_less);
+
+  if(longuest == r::end(made_up_words))
+      return {};
+  else
+      return make_optional(*longuest);
 }
 
 int main() {
-    return 0;
+  {
+      auto longuest = longuest_madeup_word( {"cat", "er", "banana", "dog", "nana", "walk", "walker", "dogwalker"});
+      assert(longuest.value() == "dogwalker");
+  }
+
+  {
+      auto longuest = longuest_madeup_word({"cat", "dog"});
+      assert(!longuest.has_value());
+  }
+
+  {
+      auto longuest = longuest_madeup_word({"cat", "dog", "gy", "doggy"});
+      assert(longuest.value() == "doggy");
+  }
+
+  {
+      auto longuest = longuest_madeup_word({"cat", "dog", "doggy"});
+      assert(!longuest.has_value());
+  }
+
+  return 0;
 }
